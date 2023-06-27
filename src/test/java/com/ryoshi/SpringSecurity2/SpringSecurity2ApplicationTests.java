99dbd275190a584feb035b0b8d52fa761ef47e88
Ryoshi <fischerklon3@gmail.com>
2023-06-27 15:13:54 +0200

    First Files

---
+++ src/test/java/com/ryoshi/SpringSecurity2/SpringSecurity2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ryoshi.SpringSecurity2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurity2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
